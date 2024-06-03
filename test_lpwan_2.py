49812bfb20f88321513885a1f1039d73e1210fb8
hachem <omhx21@gmail.com>
2024-06-03 18:23:38 +0200

    [testcase] linear, some problems with neighbour count

---
+++ test_lpwan_2.py
@@ -0,0 +1,66 @@
+from lpwan_jitter import *
+
+"""
+Linear cluster test
+"""
+
+random.seed(1)
+
+# Set loggers
+EVENT_LOGGER.set_verbose(False)
+GATEWAY_LOGGER.set_verbose(True)
+NODE_LOGGER.set_verbose(True)
+SIMULATOR_LOGGER.set_verbose(False)
+SOURCE_LOGGER.set_verbose(True)
+CHANNEL_LOGGER.set_verbose(False)
+
+def node_cluster_around(x, y, number, radius):
+    theta = 0
+    nodes = []
+    for i in range(number):
+        theta += math.pi * 2 / number
+        this_x = x + radius * math.cos(theta)
+        this_y = y + radius * math.sin(theta)
+        nodes.append(NodeLP(this_x, this_y))
+    return nodes
+
+CHANNEL_HEARING_RADIUS = 10.0
+inter_cluster_distance = 8.0
+intra_cluster_distance = 3.0
+cluster_number = 5
+number_of_clusters = 5
+final_distance = 0
+start_x = 0.0
+start_y = 0.0
+
+nodes = []
+source = SourceLP(start_x, start_y, 10)
+nodes.append(source)
+for i in range(number_of_clusters):
+    nodes.extend(node_cluster_around(start_x + inter_cluster_distance*i, start_y, cluster_number, intra_cluster_distance))
+end_x = start_x + inter_cluster_distance * number_of_clusters
+end_y = 0
+nodes.append(GatewayLP(end_x, end_y))
+
+# Example usage:
+sim = Simulator(10000, 0.001)
+
+# Create channel
+channel = Channel()
+
+# Register all nodes to channel
+channel.create_metric_mesh(CHANNEL_HEARING_RADIUS, *nodes)
+
+# Add nodes to simulator
+sim.add_nodes(*nodes)
+
+# List of nodes
+
+# Connect nodes within a certain distance
+# add_neighbors_within_distance(nodes, distance_threshold=6)
+
+# Start sending packets from source
+source.start_sending(sim)
+
+# Run the simulator
+sim.run()
